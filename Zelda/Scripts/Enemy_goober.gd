4a265f6a19fe7bb314f8964345fc9c7579d0f79b
David <davidkueng@gmail.com>
2020-05-16 16:37:18 +0200

    added shop tiles

---
+++ Zelda/Scripts/Enemy_goober.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+onready var follow_path = get_parent()
+
+var move_speed = 20
+var move_dir = 0
+var anim_player
+var hit_area
+
+func _ready():
+	anim_player = $AnimationPlayer
+
+func _physics_process(delta):
+	var prev_pos = follow_path.get_global_position()
+	follow_path.set_offset(follow_path.get_offset() + move_speed * delta)
+#	var pos = follow_path.get_global_position()
+#	move_dir = (pos.angle_to_point(prev_pos) / 3.14) * 180
+	anim_player.play("enemy_goober_walk")	
+	
+
+#func _process(delta):
+#	anim_player.play("enemy_goober_walk"
--- Zelda/Scripts/Player.gd
+++ Zelda/Scripts/Player.gd
@@ -7,7 +7,7 @@ var player_spawn_pos
 
 func _ready():
 	anim_player = $AnimationPlayer
-	tilemap = Globals.current_scene.get_node("TileMap")
+	tilemap = Globals.current_scene.get_node("Level_TileMap")
 
 func _process(delta):
 	if Input.is_action_just_pressed("exit"):
@@ -39,10 +39,12 @@ func _physics_process(delta):
 	
 	if coll:
 		if tilemap == null:
-			tilemap = $"/root/Main/Starting_World/TileMap"
+			tilemap = $"/root/Main/Starting_World/Level_TileMap"
 		
 		var cell = tilemap.world_to_map(coll.position - coll.normal)
 		var tile_id = tilemap.get_cellv(cell)
+		var tile_name = coll.collider.tile_set.tile_get_name(tile_id)
+		print(tile_name)
 		
 		if tile_id == 5:
 			Globals.goto_scene("res://Scenes/Levels/Shop.tscn")
@@ -57,9 +59,12 @@ func _physics_process(delta):
 			Globals.player_spawn_pos = player_spawn_pos
 			
 #		if tile_id == 5:
-#			Globals.move_speed = 5		
+#			Globals.move_speed = 5
 	
 func player_spawn(path):
 	player_spawn_pos = load(path).instance().get_node("PlayerSpawn").position
 	
-	
+
+func _goober_damage(body):
+		if "Player" in body.name:
+			print("take damage")
