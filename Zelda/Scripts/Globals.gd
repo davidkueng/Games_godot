f2686b95340d13c8fcbc6a2c5085cafa0fd6bb86
David <davidkueng@gmail.com>
2020-05-19 19:58:30 +0200

    remove weapon on coll

--- Zelda/Scripts/Globals.gd
+++ Zelda/Scripts/Globals.gd
@@ -4,6 +4,7 @@ var player
 var current_scene = null
 var player_spawn_pos
 var root
+var shop_weapons = []
 #var move_speed = 2.5
 
 func _ready():
@@ -18,10 +19,13 @@ func goto_scene(path):
 func _deferred_goto_scene(path):
 
 	current_scene.free()
+	
 	var s = ResourceLoader.load(path)
 	var player_instance = ResourceLoader.load("res://Scenes/Player.tscn")
+	
 	current_scene = s.instance()
 	player = player_instance.instance()
+	
 	get_tree().get_root().add_child(current_scene)
 	current_scene.add_child(player)
 	get_tree().set_current_scene(current_scene)
--- Zelda/Scripts/Player.gd
+++ Zelda/Scripts/Player.gd
@@ -8,13 +8,17 @@ var player_spawn_pos
 
 func _ready():
 	anim_player = $AnimationPlayer
+	
 	level_tilemap = Globals.current_scene.get_node("Level_TileMap")
 	objects_tilemap = Globals.current_scene.get_node("Objects_TileMap")
-#	print(level_tilemap)
+	
 	if level_tilemap == null:
 			level_tilemap = $"/root/Main/Starting_World/Level_TileMap"
 	if objects_tilemap == null:
 			objects_tilemap = $"/root/Main/Starting_World/Objects_TileMap"
+	
+#	Globals.shop_weapons = objects_tilemap
+#	print(Globals.shop_weapons)
 			
 
 func _process(delta):
@@ -31,9 +35,8 @@ func _physics_process(delta):
 	
 	if coll and coll.collider.name == "Level_TileMap":
 		
-		var level_tile_name = get_tile_name(coll, level_tilemap)#		
-		
-		print(level_tile_name)
+		var level_tile_name = get_tile_name(coll, level_tilemap)[0]
+		var cell = get_tile_name(coll, level_tilemap)[1]
 #
 		if level_tile_name == "shop_stairs_entry":
 			Globals.goto_scene("res://Scenes/Levels/Shop.tscn")
@@ -49,28 +52,21 @@ func _physics_process(delta):
 			
 	if coll and coll.collider.name == "Objects_TileMap":
 			
-#			var objects_tile_name = get_tile_name(coll, objects_tilemap)
-
-		var cell = objects_tilemap.world_to_map(coll.position - coll.normal)
-		var tile_id = objects_tilemap.get_cellv(cell)
-		var tile_name = coll.collider.tile_set.tile_get_name(tile_id)
-			
-		print(tile_id)
-#
-##		if tile_id == 5:
-#			Globals.move_speed = 5
+		var objects_tile_name = get_tile_name(coll, objects_tilemap)[0]
+		var cell = get_tile_name(coll, objects_tilemap)[1]
+		
+		if objects_tile_name == "bow":
+			clear_tile(coll, cell)
 	
 func player_spawn(path):
-	player_spawn_pos = load(path).instance().get_node("PlayerSpawn").position
-	
+	player_spawn_pos = load(path).instance().get_node("PlayerSpawn").position	
 
 #func _goober_damage(body):
 #		if "Player" in body.name:
 #			print("take damage")
 			
-			
 func player_movement():
-	var move_vec = Vector2()	
+	var move_vec = Vector2()
 #
 	if Input.is_action_pressed("move_down"):
 		move_vec += Vector2.DOWN
@@ -93,9 +89,12 @@ func player_movement():
 	move_and_collide(move_vec * move_speed)
 	
 func get_tile_name(coll, tilemap):
-		var cell = tilemap.world_to_map(coll.position - coll.normal)
-		var tile_id = tilemap.get_cellv(cell)
-		var tile_name = coll.collider.tile_set.tile_get_name(tile_id)
+	var cell = tilemap.world_to_map(coll.position - coll.normal)
+	var tile_id = tilemap.get_cellv(cell)
+	var tile_name = coll.collider.tile_set.tile_get_name(tile_id)
 		
-		return tile_name
+	return [tile_name, tile_id]
+		
+func clear_tile(coll, tile_id):
+	return coll.collider.tile_set.remove_tile(tile_id)
 		
